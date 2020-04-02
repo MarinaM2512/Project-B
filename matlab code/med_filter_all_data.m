c06ce6e1f87cc799f5a07fa317dfe75dc0d1af94
Marina Maerkovich <marina.maer2512@gmail.com>
2020-04-02 20:46:14 +0300

    finished rearranging code in functions
    window code is not complete

--- matlab code/med_filter_all_data.m
+++ matlab code/med_filter_all_data.m
@@ -1,15 +1,16 @@
 function med_filter_all_data(date,n)
-num_meas=10;
-for name = ["sit_tap", "sit_side_ancle" ,"sit_swipe_R", "sit_swipe_L"]
-    for k=1:num_meas
-        mat_name=strcat("..\measurements\",date,"\","Data_extraction_",name,num2str(k),".mat");
-        if exist(mat_name, 'file')
-            M=load(mat_name);
-            mat=M.final;
-            filtered_mat=median_data_filt(mat,n);
-            filt_mat_name=strcat("..\measurements\",date,"\","Data_extraction_",name,num2str(k),"_FILTERED",".mat");
-            save(filt_mat_name,'filtered_mat');
-        end
+textFileName= strcat("..\measurements\",date,"\","Data_extraction_","*",".mat");
+DirList = dir(fullfile(textFileName));
+listOfFiles = {DirList.name};
+for i= 1:length(listOfFiles)
+    mat_name=strcat("..\measurements\",date,"\",listOfFiles{i});
+    if(~contains(mat_name,"FILTERED"))
+        M=load(mat_name);
+        mat=M.final;
+        filtered_mat=median_data_filt(mat,n);
+        name=split(listOfFiles{i},".");
+        filt_mat_name=strcat("..\measurements\",date,"\",name{1},"_FILTERED",".mat");
+        save(filt_mat_name,'filtered_mat');
     end
 end
 end
--- matlab code/read_data_to_mat.m
+++ matlab code/read_data_to_mat.m
@@ -4,27 +4,22 @@ function read_data_to_mat(date)
 %Measurments arangement in the mat: 
 % accel_x accel_y accel_z gyro_x gyro_y gyro_z qW qX qY qZ 
 % Cal_sys Cal_gyro Cal_accel Cal_mag FSR0 FSR1 FSR2 FSR3 FSR4
-%num_meas=10;
-for name = ["sit_tap", "sit_side_ancle" ,"sit_swipe_R", "sit_swipe_L"]
-    %for k=1:num_meas
-        %textFileName= strcat("..\measurements\",date,"\",name,num2str(k),".txt");
-        textFileName= strcat("..\measurements\",date,"\",name,"*",".txt");
-        DirList     = dir(fullfile(textFileName));
-        listOfFiles = {DirList.name};
-        for file= listOfFiles
-        %if exist(textFileName, 'file')
-            s=fileread(file); 
-            %begining_idx=regexp(s,'measurments')+length('measurments')+1;%use for measurments after 30_03 
-            begining_idx=regexp(s,'Test')+length('Test')+1;
-            s=s(begining_idx:end);
-            expression=' \n';
-            splitStr = regexp(s,expression,'split');
-            writecell(splitStr,'tmp.txt')
-            final=readmatrix('tmp.txt');
-            final=final(1:end-1,1:end-1);
-            mat_name=strcat("..\measurements\",date,"\","Data_extraction_",name,num2str(k),".mat");
-            save(mat_name,'final'); 
-        end
+    textFileName= strcat("..\measurements\",date,"\","*",".txt");
+    DirList = dir(fullfile(textFileName));
+    listOfFiles = {DirList.name};
+    for i= 1:length(listOfFiles)
+        fileName=strcat("..\measurements\",date,"\",listOfFiles{i});
+        s=fileread(fileName); 
+        %begining_idx=regexp(s,'measurments')+length('measurments')+1;%use for measurments after 30_03 
+        begining_idx=regexp(s,'Test')+length('Test')+1;
+        s=s(begining_idx:end);
+        expression=' \n';
+        splitStr = regexp(s,expression,'split');
+        writecell(splitStr,'tmp.txt')
+        final=readmatrix('tmp.txt');
+        final=final(1:end-1,1:end-1);
+        name=split(listOfFiles{i},".");
+        mat_name=strcat("..\measurements\",date,"\","Data_extraction_",name{1},".mat");
+        save(mat_name,'final'); 
+    end
 end
-end
-%end
