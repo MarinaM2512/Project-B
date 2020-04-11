ca4ab0a4b26552ebc4fbed6af173214d7aa4da59
yahel <yahelsolomon@gmail.com>
2020-04-11 16:24:28 +0300

    fix segment function and comments

--- matlab code/data2timeSegments.m
+++ matlab code/data2timeSegments.m
@@ -1,24 +1,36 @@
-function [S,T]=data2timeSegments(data,times,delay)
 % data  - all data collected
 % times - time stemps [ms]
 % delay - of system   [ms]
 % S     - cell of data segmented by time
+% T     - cell of timestemp with corelation to S
+function [S,T]=data2timeSegments(data,times,delay)
 dim1=floor(times(end)/delay);
 S=cell(dim1,1);
 T=cell(dim1,1);
-end_of_frame=1;
-i=1;
+end_of_frame=[];
+i=0;
+re_ind=1;
+d_ind=20;
+remain=times;
 for k=1:dim1
-    if(i<length(data))
-       end_of_frame = find((abs(times(i:end)-times(i)-delay)<=7),1);
-    else
-        S(dim1,1)={data(i:length(data))};
-        T(dim1,1)={times(i:length(data))};
-        break
-    end
-    S(k,1)={data(i:end_of_frame)};
-    T(k,1)={times(i:end_of_frame)};
-    i=end_of_frame;
+        if(~isempty(remain(re_ind:end)))
+            remain=remain(re_ind:end);
+            diff=remain-remain(1)-delay;
+            end_of_frame = find(diff<=d_ind);
+            ind=i+end_of_frame;
+            if( ind(end) <= length(data))
+                data_tmp=data(ind);
+                time_tmp=times(ind);
+                S(k,1)={data_tmp};
+                T(k,1)={time_tmp};
+                re_ind=end_of_frame(end);
+                i=i+end_of_frame(end);       
+            else
+                S(dim1,1)={data(i:length(data))};
+                T(dim1,1)={times(i:length(data))};
+                break
+            end
+        end
 end
 end
 
--- matlab code/find_threshold_in_segment.m
+++ matlab code/find_threshold_in_segment.m
@@ -1,17 +1,25 @@
-function Flags=find_threshold_in_segment(vec,w_len,treshold,w_diff,times)
-% stop_ind=floor(length(data)/w_diff)*w_diff;
-% S,w_len,treshold,w_diff
+% Goal: to go over the data vector using timestemps and not index.
+%      Each iteration THE FUNCTION look for peaks in a window that
+%      progress in w_diff.
+% input arguments:
+% 1. vec - input data
+% 2. times - timestemps vector [sec]
+% 3. w_len - window width [sec]
+% 4. w_diff - advance the next window by w_diff [sec]
+% 5. treshold - above that value we  mark as peak [units same as vec]
+% output arguments:
+% Flags- pics after tresholding
+function Flags=find_threshold_in_segment(vec,times,w_len,w_diff,treshold)
 dim1=floor(times(end)/w_diff);
 Flags=cell(dim1,1);
 w_diff_ind=1;
 i=1;
 k=1;
-%     vec=cell2mat(S);
-%     times=cell2mat(T_seg);
-    
+
     while (w_diff_ind<=length(times))
-        
+        % transform time window into index in  matlab vector
         w_len_ind=find(abs(times(i:end)-times(i)-w_len)<=1,1);
+        % find peak
         if (w_diff_ind+w_len_ind<=length(times))
            [pks,locs]=findpeaks(vec(i:i+w_len_ind));
            w_diff_ind=find((abs(times(i:i+w_len_ind)-times(i)-w_diff)<=1),1);
