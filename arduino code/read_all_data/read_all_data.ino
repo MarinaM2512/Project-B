a3a23805eea619742e35401cb287aed5e1adbed2
Marina Maerkovich <marina.maer2512@gmail.com>
2020-04-11 17:07:24 +0300

    fix arduino code with millis

--- arduino code/read_all_data/read_all_data.ino
+++ arduino code/read_all_data/read_all_data.ino
@@ -3,37 +3,20 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-unsigned long curr_time = 0;
+#define FSR_DT_R (2)
+#define FSR_DT_W (1)
+#define BNO_DT (20)
+unsigned long BNO_time = 0;
+unsigned long FSR_time = 0;
+unsigned long FSR_done_time=0;
+unsigned long t = 0;
+bool bno_done=0;
+bool read_flag=0;
 Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
 char start_measurments =0;
 
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
 
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
 
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3-5V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
-
-/* Set the delay between fresh samples */
-//uint16_t BNO055_SAMPLERATE_DELAY_MS = 15;
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
@@ -50,12 +33,13 @@ const int ComPin = 4;
 // 3 bits--> 0-7 reading elements:
 const int AllBits[] = {s0,s1,s2};
 const int BitsNum = 3 ;
+int k=0;
 void setup(void)
 {
   Serial.begin(115200);
   Serial.println("Orientation Sensor Test"); Serial.println("");
 
-  /* Initialise the sensor */
+  /* Initialise BNO sensor */
   if (!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
@@ -64,17 +48,17 @@ void setup(void)
   }
   delay(1000);
   
+  /* Initialise FSR sensor */
   for (int k=0 ; k < BitsNum ; k++){
     pinMode(AllBits[k], OUTPUT);
   }
   pinMode(ComPin, INPUT); // Mux input
-  //maby atenuation of ADC_2_5db from ADC_0db
-  analogSetPinAttenuation( ComPin , ADC_11db); //sets the com pin attunation to 0, meaning the voltage range is 0-1v 
+  analogSetPinAttenuation( ComPin , ADC_11db); 
  // initializing :LOW to all OUTPUTS except from the first LED mode:
   for (int k=0 ; k < BitsNum ; k++){
     digitalWrite(AllBits[k], LOW);
   }
-  
+  //Calibrate before g=beginningof measurments
   uint8_t system, gyro, accel, mag = 0;
   while( system<3 || gyro<3 || accel<3 || mag<3 ){
     bno.getCalibration(&system, &gyro, &accel, &mag);
@@ -96,97 +80,62 @@ void setup(void)
     delay(300);
   }
     Serial.println("Starting measurments");
-    curr_time=0;
+    BNO_time=0;
+    FSR_time=0;
+    FSR_done_time =0;
 }
-
-
-////////////////////////////////////////MuxReadFunc//////////////////////////////////////////////////////
-int MuxReadFunc(int MuxNum){
-
-  for (int k=0 ; k < BitsNum ; k++){
-    digitalWrite(AllBits[k], bitRead(MuxNum,k));
-}
-curr_time=millis();
-while(millis()<curr_time+5);
-//delay(5);
-return ComPin;
-}
-
 
 void loop(void)
 {
-  
-  //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
-  sensors_event_t orientationData , angVelocityData , linearAccelData;
-  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
-  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
-  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  // Every BNO_DT period get a new data sample of BNO and FSR
+  if (millis() - BNO_time>= BNO_DT){
+    BNO_time = millis();
+    sensors_event_t orientationData , angVelocityData , linearAccelData;
+    bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+    bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+    bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
   
     //Linear acceleration
     double x = (linearAccelData.acceleration).x;
     double y = (linearAccelData.acceleration).y;
     double z = (linearAccelData.acceleration).z;
-    Serial.print(x);
-    //Serial.print(" accel_y: ");
-    Serial.print(" ");
-    Serial.print(y);
-    //Serial.print(" accel_z: ");
-    Serial.print(" ");
-    Serial.print(z);
+    Serial.printf("%.4lf %.4lf %.4lf",x,y,z);
 
     //Angular acceleration
 
     x = angVelocityData.gyro.x;
     y = angVelocityData.gyro.y;
     z = angVelocityData.gyro.z;
+    Serial.printf(" %.4lf %.4lf %.4lf",x,y,z);
 
-    //Serial.print(" gyro_x: ");
-    Serial.print(" ");
-    Serial.print(x);
-    //Serial.print(" gyro_y: ");
-    Serial.print(" ");
-    Serial.print(y);
-    //Serial.print(" gyro_z: ");
-    Serial.print(" ");
-    Serial.print(z);
-
-  //Get quaternions
-  imu::Quaternion quat = bno.getQuat();
-  //Serial.print(" qW: ");
-  Serial.print(" ");
-  Serial.print(quat.w(), 4);
-  //Serial.print(" qX: ");
-  Serial.print(" ");
-  Serial.print(quat.x(), 4);
-  //Serial.print(" qY: ");
-  Serial.print(" ");
-  Serial.print(quat.y(), 4);
-  //Serial.print(" qZ: ");
-  Serial.print(" ");
-  Serial.print(quat.z(), 4);
- 
- /* Display calibration status for each sensor. */
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.print(" ");
-  Serial.print(system, DEC);
-  Serial.print(" ");
-  Serial.print(gyro, DEC);
-  Serial.print(" ");
-  Serial.print(accel, DEC);
-  Serial.print(" ");
-  Serial.print(mag, DEC); 
-  
-    for(int k=0; k<5; k++){
-    int value = analogRead(MuxReadFunc(k));
-    Serial.print(" ");
-    Serial.print(value);
-    //
-      //delay of 5 ms
-      //delay(5);
+    //Get quaternions
+    imu::Quaternion quat = bno.getQuat();
+    Serial.printf(" %.4lf %.4lf %.4lf %.4lf",quat.w(),quat.x(),quat.y(),quat.z());
+    
+   /* Display calibration status for each sensor. */
+    uint8_t system, gyro, accel, mag = 0;
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    Serial.print((String)" "+system+" "+gyro+" "+accel+" "+mag);
+    bno_done=1;
+  } 
+  /* If BNO samples are finished 5-8 msec start measuring FSR 5 - 10 msec */
+    if(millis()-FSR_time >= FSR_DT_W && bno_done && !read_flag){
+    for (int i=0 ; i < BitsNum ; i++){
+      digitalWrite(AllBits[i], bitRead(k,i));
+    }
+    read_flag = 1;
   }
-  Serial.println("");
-  //delay(5);
-  curr_time=millis();
-  while(millis()<curr_time+5);
+  if (millis()- FSR_time >= FSR_DT_R && read_flag){
+      int value = analogRead(ComPin);
+      Serial.print(" ");
+      Serial.print(value);
+      k++;
+      if (k>4){
+        k=0;
+        Serial.println("");
+        bno_done=0;
+      }
+      read_flag=0;
+      FSR_time =millis();
+   }
 }
