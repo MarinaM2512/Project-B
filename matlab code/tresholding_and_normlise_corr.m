d8ab62ba10c924cd28978d96c5601c626d2dd62a
yahel <yahelsolomon@gmail.com>
2020-06-18 12:47:41 +0300

    new function- final

--- matlab code/tresholding_and_normlise_corr.m
+++ matlab code/tresholding_and_normlise_corr.m
@@ -1,18 +1,27 @@
-function corr_after_th_and_norm = tresholding_and_normlise_corr(times,corr,th,op)
+function [corr_out,diff,p] = tresholding_and_normlise_corr(corr,th,op)
 % function select peacks based on op - we want only dominant peaks of corr
 % normlise data based on th and returns peaks vec with original times
 % input:
-% times - vector -time stemps, uniform sampled
 % corr - data vector correspondes to times
 % th - thresh hold value selected based on op
 % op - str inserted to "findpeaks" and select the opration we wand to preform:
 %     'Threshold' - select peaks that exceed their immediate neighboring 
-%                   values by at least the value of 'Threshold'.
-%     'MinPeakHeight' - select those peaks higher than 'MinPeakHeight'
+%                   values by at least the value of th.
+%     'MinPeakHeight' - select those peaks higher than th.
 % output:
+% diff - error in %
+% corr_out - only peaks vector corresponds with corr times
+% p - num of peack selected
 corr_out = zeros(size(corr));
-n = max(corr);
-corr = corr/n; % normilize
-[p,ind] = findpeaks(corr,op,th);
-corr_out(ind)= p;
-diff = p-th;
+% n = max(corr);
+% corr = corr/n; % normilize
+[peak,ind] = findpeaks(corr,op,th);
+diff = zeros(size(peak));
+p = length(peak);
+corr_out(ind)= peak;
+if strcmp(op,"Threshold")
+    diff = 100*th/peak;
+elseif strcmp(op,"MinPeakHeight")
+    diff = 100*((peak-th)./peak);
+end
+end
