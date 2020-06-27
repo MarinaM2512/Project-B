18e99411b30c7bed412e9d1b7f0f06a2e2cc23d3
yahel <yahelsolomon@gmail.com>
2020-06-27 15:56:41 +0300

    inal hahaim

--- matlab code/script_test_cross_corr.m
+++
@@ -1,4 +0,0 @@
-%function label_mat = xcorr_segments_and_classify(data,times
-template_mat = loadTemplateMat;
-data_mat = loadMeasurmentMat("17_04","sit_N_tap1",1,"INIT"); %load one data mes
-seg=data2timeSegmentsOverlapping(data_mat(:,4:6),data_mat(:,20),63,60);
