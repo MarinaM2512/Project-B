9b1489b2672203786720ae72ea162b9ae66e9817
yahel <yahelsolomon@gmail.com>
2020-08-04 13:41:20 +0300

    chnged toget vec and output vec

--- matlab code/convert_bool_vec_to_times.m
+++ matlab code/convert_bool_vec_to_times.m
@@ -2,17 +2,14 @@ function [real_times,algo_times] = convert_bool_vec_to_times(real_labels,algo_la
 % func  get matrix of labels type bool size NXnum_of_move and save iv cell
 % only the times with the '1' labels.
 % INPUT:
-% algo_labels - bool array NXnum_of_movments(4)
-% real_labels - bool array NXnum_of_movments(4)
+% algo_labels - bool vec
+% real_labels - bool vec
 % times - times resampled in the raw data
 % OUTPUT:
-% real_times - times that we taged there is movement- prior
-% algo_times - times that algorithem taged there is movement
-num_of_movements = 4;
-real_times = cell(1,num_of_movements);
-algo_times = cell(1,num_of_movements);
-for i=1:num_of_movements
-    real_times{i} = times(find(real_labels(:,i)));
-    algo_times{i} = times(find(algo_labels(:,i)));
-end
+% real_times - vec times that we taged there is movement- prior
+% algo_times - vec times that algorithem taged there is movement
+
+    real_times = times(find(real_labels));
+    algo_times = times(find(algo_labels));
+    
 end
