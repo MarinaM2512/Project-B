f7f58a645c3ce54f45955f857c11d7a83d557384
Marina Maerkovich <marina.maer2512@gmail.com>
2020-08-05 13:38:35 +0300

    plot results

--- matlab code/script_plot_results.m
+++ matlab code/script_plot_results.m
@@ -3,6 +3,12 @@ clear all;
 close all;
 clc;
 %% Plot classification results
+th1 = 0.7; 
+th2 = 0.1;
+t2  = 200; %[mili-sec]
+th3= 0.7;
+hold_time = 1500;
+op1 ='MinPeakHeight';
 xcorr_data = load("./to grid search/xcorr_data.mat").val;
 times = load("./to grid search/times.mat").val;
 real_labels = load("./to grid search/real_labels.mat").val;
@@ -15,9 +21,7 @@ for j = 1:length(xcorr_data)
     curr_times = times{j};
     Ts = curr_times(2)- curr_times(1);
     hold_samp =  floor(hold_time/Ts);
-    [~,xcorr_out] = tresholding_xcorr(xcorr_data{j}.corr,curr_times,th1,'',th2,t2);
-    xcorr_out =cellfun(@(x) padarray(x,[(length(curr_times)-length(xcorr_out{1})),0],...
-        0,'post'),xcorr_out,'UniformOutput',false);
+    xcorr_out = tresholding_xcorr(xcorr_data{j}.corr,curr_times,th1,op1,th2,t2);
     algo_labels_tmp = labeling_xcorr(xcorr_out,th3,hold_samp);
     algo_labels{j} = algo_labels;
     algo_labels_relevant_move{j} = algo_labels_tmp(:,move_type);
