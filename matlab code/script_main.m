beec0138a48856bfd365549289b7b8b7c0421914
yahel <yahelsolomon@gmail.com>
2020-08-05 11:49:19 +0300

    loads

--- matlab code/script_main.m
+++ matlab code/script_main.m
@@ -2,37 +2,11 @@ clear all; close all; clc;
 
 %%
 % get data to insert grid_serch
-date = "17_04";
-endOrStart = 'start';
-move_name = get_all_meas_names(date, "FILTERED_INIT", 1);   % all names
-real_labels = get_all_real_labels(endOrStart);              % cell of labels from all meas
-% init
-xcorr_data = cell(size(move_name')); 
-times = cell(size(move_name'));
-n=0;
-% save each meas in cell
-for i=1:length(move_name) 
-        data_mat = loadMeasurmentMat(date,move_name{i},1,"INIT");%load all data of meas i
-        gyro = cat(2,data_mat(:,4:6),data_mat(:,20));            %load gyro data of meas i
-        times{i} = data_mat(:,20);                               %save time sampels of meas i
-        n=n+length(times{i});                                    %update length of total time vec
-        % cal xcorr and insert to corr fild
-        [xcorr_data{i}.corr,~] = xcorr_all_intresting_data(gyro,-1000,'normalized',"start");
-        % create and update type fild in data_xcorr
-        % swipe L - 1, swipe R - 2, tap - 3, ankle - 4, else - 1
-        if(contains(move_name{i},"swipe_L"))
-            xcorr_data{i}.type = 1;
-        elseif(contains(move_name{i},"swipe_R"))
-            xcorr_data{i}.type = 2;
-        elseif(contains(move_name{i},"tap"))
-            xcorr_data{i}.type = 3;
-        elseif(contains(move_name{i},"side_ancle"))
-            xcorr_data{i}.type = 4;
-        else
-            xcorr_data{i}.type = 1; %e.x walking
-        end
-            
-end   
+xcorr_data = load("./to grid search/xcorr_data.mat").val;
+times = load("./to grid search/times.mat").val;
+real_labels = load("./to grid search/real_labels.mat").val;
+n = load("./to grid search/n.mat").val;
+
 %% demands params to all moves
 % th1_range = [0.7 1.3]; 
 % th2_range = [0.1 0.5];
