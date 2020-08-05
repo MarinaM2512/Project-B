dfac307c3147ab3300dac556a7f917f416cdfb31
unknown <marina.maer2512@gmail.com>
2020-08-05 14:28:44 +0300

    added func for confussion mat

--- matlab code/script_main.m
+++ matlab code/script_main.m
@@ -8,14 +8,14 @@ real_labels = load("./to grid search/real_labels.mat").val;
 n = load("./to grid search/n.mat").val;
 
 %% demands params to all moves
-th1_range = [0.7 1.3]; 
-th2_range = [0.1 0.5];
-t2_range  = [200 500]; %[mili-sec]
-th3_range = [0.7 1];
-% th1_range = 0.6:0.05:1; 
-% th2_range = 0.1:0.05:0.5;
-% t2_range  = 200:10:500; %[mili-sec]
-% th3_range = 0.7:0.05:1;
+% th1_range = [0.7 1.3]; 
+% th2_range = [0.1 0.5];
+% t2_range  = [200 500]; %[mili-sec]
+% th3_range = [0.7 1];
+th1_range = 0.6:0.05:1; 
+th2_range = 0.1:0.05:0.5;
+t2_range  = 200:10:500; %[mili-sec]
+th3_range = 0.7:0.05:1;
 FPR_max   = 30; % [%]
 hold_time = 1500; %[mili-sec]
 [TPR, FPR, TNR, PPV, th1_out, th2_out, t2_out,th3_out] = ...
--- matlab code/script_plot_results.m
+++ matlab code/script_plot_results.m
@@ -8,6 +8,7 @@ th2 = 0.1;
 t2  = 200; %[mili-sec]
 th3= 0.7;
 hold_time = 1500;
+template_len = 63;
 op1 ='MinPeakHeight';
 xcorr_data = load("./to grid search/xcorr_data.mat").val;
 times = load("./to grid search/times.mat").val;
@@ -23,11 +24,16 @@ for j = 1:length(xcorr_data)
     hold_samp =  floor(hold_time/Ts);
     xcorr_out = tresholding_xcorr(xcorr_data{j}.corr,curr_times,th1,op1,th2,t2);
     algo_labels_tmp = labeling_xcorr(xcorr_out,th3,hold_samp);
-    algo_labels{j} = algo_labels;
+    algo_labels{j} = algo_labels_tmp;
     algo_labels_relevant_move{j} = algo_labels_tmp(:,move_type);
     real_labels_tmp = real_labels{j};
     real_labels_relevant_move{j} = real_labels_tmp(:,move_type);
 end
-
-%% Preformance evaluation
+[united_times,united_algo_labels,...
+                        united_real_labels] = unite(times,algo_labels,...
+                        real_labels,template_len);
 %% Confusion matrix 
+real_labels_out = add_class(united_real_labels);
+algo_labels_out = add_class(united_algo_labels);
+plotconfusion(real_labels_out',algo_labels_out');
+%% Preformance evaluation
