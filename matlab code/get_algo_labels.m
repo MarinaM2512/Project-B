2e3420c53f1faae0da0dbeb2ed61b51670332948
yahel <yahelsolomon@gmail.com>
2020-08-09 16:05:36 +0300

    after grid_serch find algo labels

---
+++ matlab code/get_algo_labels.m
@@ -0,0 +1,21 @@
+function algo_labels = get_algo_labels(th1,th2,t2,th3,hold_time)
+op1 ='MinPeakHeight';
+xcorr_data = load("./to grid search/xcorr_data.mat").val;
+times = load("./to grid search/times.mat").val;
+real_labels = load("./to grid search/real_labels.mat").val;
+real_labels_relevant_move  = cell(length(xcorr_data),1);
+algo_labels_relevant_move  = cell(length(xcorr_data),1);
+algo_labels  = cell(length(xcorr_data),1);
+for j = 1:length(xcorr_data)
+    move_type = xcorr_data{j}.type;
+    curr_times = times{j};
+    Ts = curr_times(2)- curr_times(1);
+    hold_samp =  floor(hold_time/Ts);
+    xcorr_out = tresholding_xcorr(xcorr_data{j}.corr,curr_times,th1,op1,th2,t2);
+    [~,algo_labels_tmp] = labeling_xcorr(xcorr_out,th3,hold_samp);
+    algo_labels{j}  = algo_labels_tmp;
+    algo_labels_relevant_move{j} = algo_labels_tmp(:,move_type);
+    real_labels_tmp = real_labels{j};
+    real_labels_relevant_move{j} = real_labels_tmp(:,move_type);
+end
+end
