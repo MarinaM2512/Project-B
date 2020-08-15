2dd81e45eda85449e98b7715af8ac5978c4dc09e
Marina Maerkovich <marina.maer2512@gmail.com>
2020-08-15 21:43:38 +0300

    Loading optimal parameters

--- matlab code/script_main.m
+++ matlab code/script_main.m
@@ -8,7 +8,6 @@ n = load("./to grid search/n.mat").val;
 template_len = 63;
 %% demands params to all moves
 th1_range = 0.5:0.05:0.9; 
-%th2_range = 0.4:0.05:0.8;
 th2_range = [];
 t2_range  = 60:20:300; %[mili-sec]
 th3_range = 0.6:0.05:0.9;
@@ -18,7 +17,11 @@ hold_time = 1500; %[mili-sec]
 [TPR, FPR, TNR, PPV, th1_out, th2_out, t2_out,th3_out] = ...
     grid_search(...
     real_labels,n,xcorr_data,times,th1_range,th2_range,t2_range,th3_range,FPR_max,hold_time);
-
+%% Load optimal results 
+th1_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th1;
+th2_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th2;
+th3_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th3;
+t2_out = load("./results after grid search/14-Aug-2020/thresholds.mat").t2;
 %% plot optimal results
 [algo_labels,algo_vals] = get_algo_labels(th1_out, th2_out, t2_out,th3_out,hold_time);
 plot_results_stem(algo_vals);
