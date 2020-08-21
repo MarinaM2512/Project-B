11e711fdfe6114a45c57289576af181124de1b93
yahel <yahelsolomon@gmail.com>
2020-08-21 12:27:47 +0300

    updatet

--- matlab code/script_main.m
+++ matlab code/script_main.m
@@ -20,11 +20,11 @@ hold_time = 1500; %[mili-sec]
 %% Load optimal results 
 th1_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th1;
 th2_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th2;
-th3_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th3;
 t2_out = load("./results after grid search/14-Aug-2020/thresholds.mat").t2;
+th3_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th3;
 %% plot optimal results
 hold_time = 1500; %[mili-sec]
-[algo_labels,algo_vals] = get_algo_labels(th1, th2, t2,th3,hold_time);
+[algo_labels,algo_vals] = get_algo_labels(th1_out, th2_out, t2_out,th3_out,hold_time);
 %plot_results_stem(algo_vals);
 %% Confusion matrix 
 [united_times,united_algo_labels,united_real_labels] =...
