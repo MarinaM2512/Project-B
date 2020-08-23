0856b3ef00f989a59ab2ad1d7d67f446400e3818
yahel <yahelsolomon@gmail.com>
2020-08-23 14:55:10 +0300

    DEMO
    function needded fot demo script

--- demo code/DEMO.m
+++ demo code/DEMO.m
@@ -1,15 +1,31 @@
 %% DEFINE 
 % date = ""; 
 % path = ""; 
+endOrStart = 'start';
 template_len = 63; %[samples] 
 hold_time = 1500; %[mili-sec] 
- 
-%% LOADS 
-% thresholds 
+%% LOAD thresholds 
 th1_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th1; 
 th2_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th2; 
 th3_out = load("./results after grid search/14-Aug-2020/thresholds.mat").th3; 
-t2_out = load("./results after grid search/14-Aug-2020/thresholds.mat").t2; 
-% labels 
+t2_out = load("./results after grid search/14-Aug-2020/thresholds.mat").t2;
+%% XCORR
+move_name = get_all_meas_names(date, "FILTERED_INIT", 1);   % all names
+% init
+xcorr_data = cell(size(move_name')); 
+times = cell(size(move_name'));
+n1=0;
+% each meas in cell
+for i=1:length(move_name) 
+        data_mat = loadMeasurmentMat(date,move_name{i},1,"INIT");%load all data of meas i
+        gyro = cat(2,data_mat(:,4:6),data_mat(:,20));            %load gyro data of meas i
+        times{i} = data_mat(:,20);                               %save time sampels of meas i
+        n1=n1+length(times{i});                                    %update length of total time vec
+        % cal xcorr and insert to corr fild
+        [xcorr_data{i}.corr,~] = xcorr_all_intresting_data(gyro,'normalized',"start");
+
+         
+end   
+%% labels 
 real_labels = load("./to grid search/real_labels.mat").val; 
 [algo_labels,algo_vals] = get_algo_labels_new(xcorr_data,times,th1_out, th2_out, t2_out,th3_out,hold_time); 
--- demo code/get_algo_labels_new.m
+++ demo code/get_algo_labels_new.m
@@ -1,4 +1,23 @@
-function [algo_labels,algo_labels_val] = get_algo_labels_new(xcorr_data,times,th1,th2,t2,th3,hold_time) 
+function [algo_labels,algo_labels_val] = get_algo_labels_new(...
+                xcorr_data,times,th1,th2,t2,th3,hold_time) 
+% function applies thresholding on xcorr result and labels it.
+% The treshold values taken from grid_search
+% INPUT:
+% 1. xcorr_data - cell array for all measurments each cell containing -a struct:
+%    field corr cross corelation of shape: Nx3x4 , where N is the measurment
+%    length , 3 channels - x,y,z, 4 templates - "swipe left" , "swipe right" ,
+%    "tap" & "ankle". field type is - 1 (for swipe left), 2 (for swipe right),
+% 3 (for tap), 4 (for side ankle).
+% 2. times - the times of the measurment arranged in a cell array with each
+%    cell containing the time vector of each measurment.
+% 3,4. th1=th2 - select peaks above th1
+% 5. t2 - select peaks if they last t2 sec above th1
+% 6. th3 - threshold appliyed after weighing xcorr.
+% 7. hold_time - maximal time difference in samples in which we coose the
+% largest peak for the final result.
+% Ouput:
+% labels - a Nx4 binary matrix which contains 1's at the vector locations
+% of the start of a movement
 op1 ='MinPeakHeight'; 
 algo_labels  = cell(length(xcorr_data),1); 
 algo_labels_val  = cell(length(xcorr_data),1); 
