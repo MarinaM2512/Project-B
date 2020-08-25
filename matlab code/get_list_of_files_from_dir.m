6c3beb4366a4b8b9300bf4b073bbbcb657031545
yahel <yahelsolomon@gmail.com>
2020-08-25 14:33:22 +0300

    comments

--- matlab code/get_list_of_files_from_dir.m
+++ matlab code/get_list_of_files_from_dir.m
@@ -1,4 +1,18 @@
 function listOfFiles = get_list_of_files_from_dir (DirPath ,date, measType)
+% function get name list of all diffrenet measurments in th directory
+% "DirPath/date" 
+% INPUT: 
+% 1. DirPath - path directory all measurments are saved
+% 2. date - directory in DirPath where the data collected in date was saved 
+% "DirPath/date" is the Directory Path where raw data is saved
+% 3. measType - can get the following:
+%            a. "" : empty string - to get listOfFiles for raw data
+%            b. "FILTERED_INIT" : to get listOfFiles of the filtered
+%            measurments
+% OUTPUT:
+% listOfFiles - cell of srings 
+%               contains all names of diffrenet measurments collected on date
+
     if(~strcmp(measType,""))
         textFileName= strcat(DirPath,"\",date,"\","*",measType,"*.mat");
     else
