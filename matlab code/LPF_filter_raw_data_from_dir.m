347498c9e890bc746a0eed24b736cf1372276d99
yahel <yahelsolomon@gmail.com>
2020-08-25 14:31:53 +0300

    comments

--- matlab code/LPF_filter_raw_data_from_dir.m
+++ matlab code/LPF_filter_raw_data_from_dir.m
@@ -1,12 +1,23 @@
 function filtered = LPF_filter_raw_data_from_dir(DirPath,date , Fpass)
+% function resamples all measurments in directory and uses lowpass Matlab 
+% function.
+% INPUT: 
+% 1. DirPath - path directory all measurments are saved
+% 2. date - directory in DirPath where the data collected in date was saved 
+% "DirPath/date" is the path directory raw data is saved
+% 3. Fpass - pass band frequency of the LPF[Hz]
+% OUTPUT:
+% 1. filtered - cell array  of length as num of movements in the directory
+%               each cell contains resampled and filtered data
+
 listOfFiles = get_list_of_files_from_dir(DirPath ,date, "");
-filtered = cell(length(listOfFiles),1);
+filtered = cell(length(listOfFiles));
 for i= 1:length(listOfFiles)
     mat_name=strcat(DirPath,"\",date,"\",listOfFiles{i});
-    if(~contains(mat_name,"FILTERED"))
-        M=load(mat_name);
-        mat=M.final;
-        resampled = resample_all_data(mat);
+    if(~contains(mat_name,"FILTERED"))            % if not filtered
+        M = load(mat_name);
+        mat = M.final;
+        resampled = resample_all_data(mat);       % resample
         t = resampled(:,20);
         Fs = 1000/(t(2)-t(1));
         filtered{i} = [lowpass(resampled(:,1:19),Fpass,Fs) t]; 
--- matlab code/resample_and_filter_raw_data.m
+++ matlab code/resample_and_filter_raw_data.m
@@ -1,4 +1,18 @@
 function resample_and_filter_raw_data(DirPath, date,n,type)
+% function get Directory Path where raw data is saved
+% INPUT: 
+% 1. DirPath - path directory all measurments are saved
+% 2. date - directory in DirPath where the data collected in date was saved 
+% "DirPath/date" is the Directory Path where raw data is saved
+% 3. n - order of the one-dimensional median filter. The center of the med
+%        filt is in floor(0.5n)+1 ( see n in help for Matlab func medfilt1)
+%        this param is used only when type is "MEDIAN"
+% filter is 
+% 4. type - type of filter used on data
+%           can be one of the following:
+%           a. "LPF" - uses function "LPF_filter_raw_data_from_dir"
+%                      this func uses lowpass filter of matlab.
+%           b. "MEDIAN" - 
     listOfFiles = get_meas_names_from_dir(DirPath,date,"");
     mat_full_names = get_list_of_files_from_dir (DirPath,date,"");
     all_data_filetered = LPF_filter_raw_data_from_dir(DirPath,date , n);
@@ -6,12 +20,12 @@ function resample_and_filter_raw_data(DirPath, date,n,type)
         if(~contains(mat_full_names{i},"FILTERED"))
             switch type
                 case "LPF"
-                    data_filetered = all_data_filetered{i};
+                    filtered_mat = all_data_filetered{i};
                 case "MEDIAN"
                     data_mat = load_measurment_mat_from_dir(DirPath,date,listOfFiles{i},"");
                     data_filetered=median_data_filt(data_mat,n);
+                    filtered_mat = resample_all_data(data_filetered);
             end            
-            filtered_mat = resample_all_data(data_filetered);
             name=split(mat_full_names{i},".");
             mat_full_name = name{1};
             mat_name=strcat(DirPath,"\",date,"\",mat_full_name,"_FILTERED",".mat");
